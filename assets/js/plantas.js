22fd3da48ed9f3febe16ab9c78058c6d9f50a218
Andrea <86671457+AndreaChavezMunoz@users.noreply.github.com>
2024-12-06 16:46:24 -0500

    tamaños divididos en bullet points

--- assets/js/plantas.js
+++ assets/js/plantas.js
@@ -1,3 +1,5 @@
+import { crearTabla } from "./plantasData.js";
+
 document.addEventListener('DOMContentLoaded', async () => {
 
     // Variables de la página
@@ -10,6 +12,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             const datosCSV = await obtenerDatosCSV('assets/data/plantas.csv');
             const datosFiltrados = filtrarDatos(datosCSV);
             const datosDivididos = transformarColumnasEnListas(datosFiltrados);
+            console.log(datosDivididos);
             tablaPlantas = crearTabla(datosDivididos);
             await inicializarTabla();
         } catch (error) {
@@ -40,63 +43,25 @@ document.addEventListener('DOMContentLoaded', async () => {
         return datos.filter(row => Object.values(row).some(value => value.trim() !== ''));
     }
 
-    // Crear la tabla de Tabulator
-    function crearTabla(datos) {
-        return new Tabulator("#plantasTabla", {
-            data: datos,
-            layout: "fitColumns",
-            responsiveLayout: "hide",
-            pagination: "local",
-            paginationSize: 10,
-            movableColumns: true,
-            resizableRows: true,
-            columns: obtenerColumnasTabla(),
-        });
-    }
-
-    // Definir columnas para la tabla
-    function obtenerColumnasTabla() {
-        return [
-            { title: "Familia", field: "Familia" },
-            { title: "Género", field: "Genero" },
-            { title: "Nombre Científico", field: "Nombre Cientifico" },
-            { title: "Nombre Común", field: "Nombre comun" },
-            { title: "Categoría", field: "Categoria" },
-            { title: "Variedades", field: "Variedades", visible: false },
-            { title: "Tamaño", field: "Tamano" },
-            { title: "Hábitat", field: "Habitat",
-                formatter: function(cell) {
-                    const values = cell.getValue(); // Obtener el valor de la celda (que es ahora un array)
-                    const ul = document.createElement('ul'); // Crear una lista desordenada (ul)
-            
-                    values.forEach(value => {
-                        const li = document.createElement('li'); // Crear un item de lista (li)
-                        li.textContent = value; // Asignar el valor del item
-                        ul.appendChild(li); // Añadir el item a la lista
-                    });
-            
-                    return ul; // Devolver la lista para renderizarla
-                }
-             },
-            { title: "Precio por Unidad (S/.)", field: "Precio por Unidad (S/)", visible: false },
-            { title: "Precio por Docena (S/.)", field: "Precio por Docena  (S/)", visible: false },
-            { title: "Precio por Ciento (S/.)", field: "Precio por Ciento  (S/)", visible: false },
-            { title: "Precio por Millar (S/.)", field: "Precio por Millar  (S/)", visible: false },
-            { title: "Estado de Stock", field: "Estado de Stock" },
-            { title: "Observación", field: "Observacion", visible: false },
-        ];
-    }
-
+    
     // Transformar la columna "Hábitat" en una lista
     function transformarColumnasEnListas(datos) {
+
+        // Columnas a transformar en listas
+        const columnasConListas = ["Tamano", "Habitat"];
+    
         return datos.map(row => {
-            // Convertir "Habitat" en un array de valores separados por "/"
-            if (row.Habitat) {
-                row.Habitat = row.Habitat.split('/').map(item => item.trim());
-            }
+            // Iterar sobre las columnas que deben ser listas
+            columnasConListas.forEach(columna => {
+                if (row[columna]) {
+                    // Dividir el contenido de la columna por un delimitador y crear un array
+                    row[columna] = row[columna].split('/').map(item => item.trim());
+                }
+            });
             return row;
         });
     }
+    
 
     // Inicializar la tabla y configurar eventos
     async function inicializarTabla() {
